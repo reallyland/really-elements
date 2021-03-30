b5dfb9e5d7c7e10ac1b04afa337557f994d6ffe9
Rong Sen Ng <wes.ngrongsen@gmail.com>
2021-03-30 23:42:25 +0800

    chore: update dependencies, fix publish files

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 import './clipboard-copy/clipboard-copy.js';
+import './clipboard-copy/really-clipboard-copy.js';
 import './code-configurator/code-configurator.js';
+import './code-configurator/really-code-configurator.js';
