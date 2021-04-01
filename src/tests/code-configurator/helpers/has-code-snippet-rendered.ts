939d3a2cfca0d37836a8622b7acfafdf043f68d0
Rong Sen Ng <wes.ngrongsen@gmail.com>
2021-04-01 23:17:15 +0800

    test: fix tests

--- src/tests/code-configurator/helpers/has-code-snippet-rendered.ts
+++ src/tests/code-configurator/helpers/has-code-snippet-rendered.ts
@@ -1,8 +1,8 @@
 export function hasCodeSnippetRendered(node: HTMLElement): boolean {
   const $ = (e: string) => node.shadowRoot?.querySelector<HTMLElement>(e);
   const h2CodeSnippet = $('.all-code-snippets-container > h2');
-  const h3Properties = $('.all-code-snippets-container > h3.properties');
-  const h3CssProperties = $('.all-code-snippets-container > h3.css-properties');
+  const h3Properties = $('.all-code-snippets-container h3.properties');
+  const h3CssProperties = $('.all-code-snippets-container h3.css-properties');
 
   return h2CodeSnippet != null && h3Properties != null && h3CssProperties != null;
 }
