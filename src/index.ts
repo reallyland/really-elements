a40682bb022c7084649be73ee1f5043f3f5e7108
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2022-01-10 22:12:38 +0800

    feat: export typings

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ import './clipboard-copy/clipboard-copy.js';
 import './clipboard-copy/really-clipboard-copy.js';
 import './code-configurator/code-configurator.js';
 import './code-configurator/really-code-configurator.js';
+
+export type { CopyError, CopySuccess } from './clipboard-copy/types';
+export type { PropertyValue, PropertyValueOptions } from './code-configurator/types';
