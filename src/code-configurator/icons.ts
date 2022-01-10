adace80d46501748c677ca1fd259d1fdd47eabad
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2022-01-10 22:54:52 +0800

    chore: cleanup

--- src/code-configurator/icons.ts
+++ src/code-configurator/icons.ts
@@ -1,10 +1,8 @@
-import { html } from 'lit-element';
+import { html } from 'lit';
 
-// tslint:disable: max-line-length
-export const contentCopy = html`<svg width="24" height="24" viewBox="0 0 24 24">
-  <path d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z" />
-</svg>`;
 export const contentCopied = html`<svg width="24" height="24" viewBox="0 0 24 24">
   <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
 </svg>`;
-// tslint:enable: max-line-length
+export const contentCopy = html`<svg width="24" height="24" viewBox="0 0 24 24">
+  <path d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z" />
+</svg>`;
