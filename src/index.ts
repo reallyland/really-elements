dbd14a715e413dc00bec1deb35a8f0c34c623e24
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2022-01-10 22:22:11 +0800

    fix: use export * from instead of import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import './clipboard-copy/clipboard-copy.js';
-import './clipboard-copy/really-clipboard-copy.js';
-import './code-configurator/code-configurator.js';
-import './code-configurator/really-code-configurator.js';
-
+export * from './clipboard-copy/clipboard-copy.js';
+export * from './clipboard-copy/really-clipboard-copy.js';
 export type { CopyError, CopySuccess } from './clipboard-copy/types';
+export * from './code-configurator/code-configurator.js';
+export * from './code-configurator/really-code-configurator.js';
 export type { PropertyValue, PropertyValueOptions } from './code-configurator/types';
