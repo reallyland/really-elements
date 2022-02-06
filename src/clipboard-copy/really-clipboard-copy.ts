f07921c94b5336a3af6c70ff12d37ee3f4383062
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2022-02-06 23:22:55 +0800

    chore: fix linting issue

--- src/clipboard-copy/really-clipboard-copy.ts
+++ src/clipboard-copy/really-clipboard-copy.ts
@@ -2,7 +2,7 @@ import { customElement } from 'lit/decorators.js';
 
 import { ClipboardCopy } from './clipboard-copy.js';
 import { localName } from './constants.js';
-import type { CopyError,CopySuccess } from './types.js';
+import type { CopyError, CopySuccess } from './types.js';
 
 @customElement(localName)
 export class ReallyClipboardCopy extends ClipboardCopy {}
