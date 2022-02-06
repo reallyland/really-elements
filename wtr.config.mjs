fae0889f72d20c089d378a5119c383c065ba935c
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2022-02-06 23:20:20 +0800

    ci: improve CI

--- wtr.config.mjs
+++ wtr.config.mjs
@@ -1,11 +1,21 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { esbuildPlugin } from '@web/dev-server-esbuild';
+import { fromRollup } from '@web/dev-server-rollup';
 import { playwrightLauncher } from '@web/test-runner-playwright';
 
 import { browserPermissions } from './wtr-plugins/browser-permissions.mjs';
 import { pageClick } from './wtr-plugins/page-click.mjs';
 import { pageFill } from './wtr-plugins/page-fill.mjs';
 
-const isCI = process.env.CI === true || process.env.CI === 'true';
+const nodeResolvePlugin = fromRollup(nodeResolve);
+
+const {
+  CI = 'false',
+  COVERAGE = 'false',
+} = process.env;
+
+const isCI = CI === 'true';
+const isCoverage = COVERAGE === 'true';
 
 /** @type {import('@web/test-runner').TestRunnerConfig} */
 const config = {
@@ -17,21 +27,25 @@ const config = {
   browserStartTimeout: 60e3,
   concurrency: 3,
   concurrentBrowsers: 9,
-  coverage: true,
-  coverageConfig: {
-    report: true,
-    threshold: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
-    },
-    nativeInstrumentation: true,
-    exclude: [
-      './src/*tests*/**',
-      'node_modules/**'
-    ],
-  },
+  ...(
+    isCoverage && ({
+      coverage: true,
+      coverageConfig: {
+        report: true,
+        threshold: {
+          branches: 80,
+          functions: 80,
+          lines: 80,
+          statements: 80
+        },
+        nativeInstrumentation: true,
+        exclude: [
+          './src/*tests*/**',
+          'node_modules/**'
+        ],
+      },
+    })
+  ),
   files: [
     // './src/tests/**/*.test.ts',
 
@@ -47,12 +61,12 @@ const config = {
     './src/tests/code-configurator/custom-event.spec.ts',
     './src/tests/code-configurator/misc.spec.ts',
   ],
-  nodeResolve: true,
   plugins: [
-    esbuildPlugin({
-      ts: true,
-      target: 'firefox82', /** FF82 does not support private fields */
+    nodeResolvePlugin({
+      exportConditions: ['default', 'dev', 'development', 'esbuild', 'import'],
+      extensions: ['.mjs', '.js', '.ts', '.css', '.graphql'],
     }),
+    esbuildPlugin({ ts: true }),
     browserPermissions(),
     pageClick(),
     pageFill(),
