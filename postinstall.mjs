ba153d99dadacbbaa9e9f2a43c4cb00806f6cb68
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2022-03-19 00:18:17 +0800

    build: update postinstall script
    Signed-off-by: Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>

--- postinstall.mjs
+++ postinstall.mjs
@@ -11,13 +11,11 @@ if (CI !== 'true') {
   } = JSON.parse(await fs.readFile('./package.json', { encoding: 'utf-8' }));
 
   if (
-    [
+    ![
       moduleName,
       moduleName.split('/', 2)[1]
-    ].some(n => (
-      !INIT_CWD.endsWith(`node_modules/${n}`) &&
-      INIT_CWD.endsWith(n)
-    ))
+    ].some(n => INIT_CWD.endsWith(`node_modules/${n}`)) &&
+    INIT_CWD.endsWith(n)
   ) {
     /**
      * NOTE: To skip running `simple-git-hooks` in CI environment.
